063390353bb71807d23bfa4d400bfb52834a0a47
Adeyemi Akinyemi <89089354+AdeDevs@users.noreply.github.com>
2023-01-18 12:46:30 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <HomePage msg="With AltSchool" />
+  </div>
+</template>
+
+<script>
+import HomePage from "./components/HomePage.vue";
+
+export default {
+  name: "App",
+  components: {
+    HomePage,
+  },
+};
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+
+#app,
+.app-container {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 10px;
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h2>Data Fetching {{ msg }}</h2>
+    <p class="joke">{{ data.joke }}</p>
+    <!-- <button @click="toggleJoke()">Next Joke</button> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      data: " ",
+    };
+  },
+  mounted() {
+    fetch("https://icanhazdadjoke.com/", {
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => (this.data = data));
+  },
+  // methods: {
+  //   toggleJoke() {
+  //     this.data = this.data.joke;
+  //   },
+  // },
+};
+</script>
+
+<style>
+.joke {
+  width: 50%;
+  margin: 15px auto;
+  padding: 15px;
+  background-color: gold;
+}
+
+button {
+  padding: 5px 15px;
+  text-transform: uppercase;
+  font-family: inherit;
+  border: 2px solid red;
+  border-radius: 5px;
+  background: transparent;
+}
+
+button:hover {
+  background: red;
+  border: 2px solid black;
+  transition: 300ms;
+}
+</style>
